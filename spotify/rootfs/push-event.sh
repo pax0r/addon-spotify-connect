18e9a79b54fbaa6c05b216d51426d16e096743e6
Bartlomiej Biernacki <pax0r@o2.pl>
2021-01-11 20:34:36 +0100

    Fix slashes

--- spotify/rootfs/push-event.sh
+++ spotify/rootfs/push-event.sh
@@ -5,11 +5,11 @@ bashio::log.info "Event from spotifyd: ${PLAYER_EVENT} ${TRACK_ID} ${OLD_TRACK_I
 case ${PLAYER_EVENT} in
   "play")
     data=$(bashio::var.json track_id "${TRACK_ID}")
-    bashio::api.supervisor "POST" "core/api/events/hassio_spotify_play" "${data}" ;;
+    bashio::api.supervisor "POST" "/core/api/events/hassio_spotify_play" "${data}" ;;
   "change")
     data=$(bashio::var.json track_id "${TRACK_ID}" old_track_id "${OLD_TRACK_ID}")
-    bashio::api.supervisor "POST" "core/api/events/hassio_spotify_change" "${data}" ;;
+    bashio::api.supervisor "POST" "/core/api/events/hassio_spotify_change" "${data}" ;;
   "stop")
     data=$(bashio::var.json track_id "${TRACK_ID}")
-    bashio::api.supervisor "POST" "core/api/events/hassio_spotify_stop" "${data}" ;;
+    bashio::api.supervisor "POST" "/core/api/events/hassio_spotify_stop" "${data}" ;;
 esac
